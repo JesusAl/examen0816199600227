b2f860e6e75285952cb6e9cdc424e5cf4da94d87
JesusAl <lainez_jesus13@hotmail.com>
2017-06-06 19:49:03 -0600

    Examen

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/usuarios', function(req,res,next){
+  var usuarios = [
+                {"user":"admin","rol":["admin","public"]},
+                {"user":"any","rol":["public"]},
+              ];
+  res.json(usuarios);
+});
+
+router.get('/suma', function(req,res,next){
+  res.render('numero1',{"resul":"","msg":""});
+});
+
+router.post('/suma', function(req, res, next){
+  console.log(req.body);
+    resultado.push(req.body.fsuma());
+
+
+});
+
 module.exports = router;
